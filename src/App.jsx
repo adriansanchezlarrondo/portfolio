2c4512b50aaeba461d059d998163029b0343da0d
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-02-19 19:57:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import tailwindLogo from './assets/tailwind.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <div>
+        <img src={tailwindLogo} className="logo" alt="Tailwind logo" />
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
