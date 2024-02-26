be6bfa9a2ff920545a56a293dac039e972cc7362
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-02-26 20:32:35 +0100

    header

--- src/componentes/Avatar.jsx
+++ src/componentes/Avatar.jsx
@@ -1,10 +1,10 @@
 function Avatar(){
     return(
     <div className="flex ml-4 mt-4">
-        <div className="w-10 h-10 rounded-full overflow-hidden">
-            <img src="./src/assets/react.svg" alt="Avatar" className="object-cover w-full h-full" />
+        <div className="w-14 h-14 rounded-full overflow-hidden">
+            <img src="./src/assets/wily.jfif" alt="Avatar" className="object-cover w-full h-full" />
         </div>
-        <h1 className="mt-2 ml-4 text-lg font-bold  hover:text-purple-700 transition duration-500">Adrián Sánchez</h1>
+        <h1 className="mt-2 ml-4 text-base text-gray-700 font-bold pt-1.5 hover:text-purple-600 transition duration-500">ADRIÁN SÁNCHEZ</h1>
     </div>
     )
 }
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,13 +3,13 @@ import Avatar from "./Avatar"
 
 function Header(){
     return (
-        <div className="text-2xl flex">
+        <div className="flex justify-around p-2">
             <Avatar />
             <div className="flex justify-end w-3/4 mt-6 space-x-12">
-                <a href="#" className="text-lg font-bold hover:text-purple-700 transition duration-500">HOME</a>
-                <a href="#" className="text-lg font-bold hover:text-purple-700 transition duration-500">ABOUT</a>
-                <a href="#" className="text-lg font-bold hover:text-purple-700 transition duration-500">PROJECTS</a>
-                <a href="#" className="text-lg font-bold hover:text-purple-700 transition duration-500">CONTACT</a>
+                <a href="#" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">HOME</a>
+                <a href="#" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">ABOUT</a>
+                <a href="#" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">PROJECTS</a>
+                <a href="#" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">CONTACT</a>
             </div>
         </div>
     )
