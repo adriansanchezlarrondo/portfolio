e43db81d9e7f9c88214d0a9569ca959fa8e57d4a
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-03-04 18:10:29 +0100

    header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,13 +3,15 @@ import Avatar from "./Avatar"
 
 function Header(){
     return (
-        <div className="flex justify-around py-4">
-            <Avatar />
-            <div className="flex justify-end w-3/4 mt-3 space-x-12">
-                <a href="#home" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">HOME</a>
-                <a href="#about" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">ABOUT</a>
-                <a href="#projects" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">PROJECTS</a>
-                <a href="#contact" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">CONTACT</a>
+        <div className="fixed top-0 w-full bg-white shadow-lg z-10">
+            <div className="flex justify-around py-4">
+                <Avatar />
+                <div className="flex justify-end w-3/4 mt-3 space-x-12">
+                    <a href="#home" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">HOME</a>
+                    <a href="#about" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">ABOUT</a>
+                    <a href="#projects" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">PROJECTS</a>
+                    <a href="#contact" className="text-base text-gray-700 font-bold hover:text-purple-600 transition duration-500">CONTACT</a>
+                </div>
             </div>
         </div>
     )
