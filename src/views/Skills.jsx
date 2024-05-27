315a6a8d31f8ea3e071afdf67261c5223cc38176
adriansanchezlarrondo <sanchezlarrondoadrian@gmail.com>
2024-05-27 14:47:52 +0200

    .

--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -3,28 +3,28 @@ export default function Skills() {
     return (
         <div className="container mx-auto my-5">
             <div className="md:flex md:justify-around items-center">
-                <img src="/HTML5.png" alt="logo-html" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/JavaScript.png" alt="logo-javascript" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/CSS3.png" alt="logo-css" className="w-[100px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/React.webp" alt="logo-react" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/Node.png" alt="logo-nodejs" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
+                <img src="/HTML5.png" alt="logo-html" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/JavaScript.png" alt="logo-javascript" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/CSS3.png" alt="logo-css" className="w-[100px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/React.webp" alt="logo-react" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/Node.png" alt="logo-nodejs" className="w-[140px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
             </div>
             <div className="md:flex md:space-y-14 md:justify-evenly items-center">
-                <img src="/Wordpress.png" alt="logo-wordpress" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/Bootstrap.png" alt="logo-bootstrap" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/Tailwind.png" alt="logo-tailwind" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/api.png" alt="logo-api" className="w-[200px] h-full my-16 mx-auto md:my-10 md:mx-0"/>
+                <img src="/Wordpress.png" alt="logo-wordpress" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/Bootstrap.png" alt="logo-bootstrap" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/Tailwind.png" alt="logo-tailwind" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/api.png" alt="logo-api" className="w-[200px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
             </div>
             <div className="md:flex md:space-y-14 md:justify-center md:space-x-52 items-center">
-                <img src="/mysql.png" alt="logo-mysql" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0"/>
-                <img src="/postgresql.png" alt="logo-postgre" className="w-[130px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/supabase.png" alt="logo-supabase" className="w-[200px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
+                <img src="/mysql.png" alt="logo-mysql" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/postgresql.png" alt="logo-postgre" className="w-[130px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/supabase.png" alt="logo-supabase" className="w-[200px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
             </div>
             <div className="md:flex md:space-y-14 md:justify-evenly items-center">
-                <img src="/PHP.png" alt="logo-php" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/Java.png" alt="logo-java" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/django-python.png" alt="logo-python-django" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
-                <img src="/C++.png" alt="logo-c++" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0  "/>
+                <img src="/PHP.png" alt="logo-php" className="w-[150px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/Java.png" alt="logo-java" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/django-python.png" alt="logo-python-django" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+                <img src="/C++.png" alt="logo-c++" className="w-[120px] h-full my-16 mx-auto md:my-10 md:mx-0 hover:scale-110 transition-transform duration-300 ease-in-out"/>
             </div>
         </div>
     )
--- src/views/Works.jsx
+++ src/views/Works.jsx
@@ -10,36 +10,44 @@ export default function Works() {
             <div className="flex flex-col items-center md:items-stretch space-y-16 mt-10 md:mt-20">
                 <div className="space-y-16 md:flex md:justify-around md:space-y-0">
                     <div className="rounded-full shadow-xl shadow-green-300 md:shadow-none hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <a href="https://gesti-n-de-incidencias.onrender.com/" target="_blank" rel="noopener noreferrer">
-                            <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
-                                <img src="/incidencias.png" alt="proyecto" className="p-2 w-[180px] md:w-[250px] shadow-xl md:shadow-2xl rounded-full" />
+                        <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
+                            <a href="https://gesti-n-de-incidencias.onrender.com/" target="_blank" rel="noopener noreferrer">
+                                <img src="/incidencias.png" alt="proyecto-incidencias" className="p-2 w-[180px] md:w-[250px] shadow-xl md:shadow-2xl rounded-full" />
+                            </a>
+                            <a href="https://github.com/adriansanchezlarrondo/Gesti-n-de-incidencias" target="_blank" rel="noopener noreferrer">
                                 <p className="text-center text-xl font-semibold w-full md:text-2xl md:mt-4">Gestión de incidencias</p>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                     <div className="rounded-full shadow-xl shadow-green-300 md:shadow-none hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <a href="https://memory-0dxa.onrender.com" target="_blank" rel="noopener noreferrer">
-                            <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
-                                <img src="/memory.png" alt="proyecto" className="p-2 w-[180px] h-[172px] md:w-[250px] md:h-[240px] shadow-xl md:shadow-2xl rounded-full" />
+                        <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
+                            <a href="https://memory-0dxa.onrender.com" target="_blank" rel="noopener noreferrer">
+                                <img src="/memory.png" alt="proyecto-memory" className="p-2 w-[180px] h-[172px] md:w-[250px] md:h-[240px] shadow-xl md:shadow-2xl rounded-full" />
+                            </a>
+                            <a href="https://github.com/adriansanchezlarrondo/Memory" target="_blank" rel="noopener noreferrer">
                                 <p className="text-center text-xl font-semibold w-full md:text-2xl md:mt-4">Memory</p>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                     <div className="rounded-full shadow-xl shadow-green-300 md:shadow-none hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <a href="https://sred-pi.vercel.app/" target="_blank" rel="noopener noreferrer">
-                            <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
-                                <img src="/sred.png" alt="proyecto" className="p-2 w-[180px] h-[172px] md:w-[250px] md:h-[240px] shadow-xl md:shadow-2xl rounded-full" />
+                        <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
+                            <a href="https://sred-pi.vercel.app/" target="_blank" rel="noopener noreferrer">
+                                <img src="/sred.png" alt="proyecto-sred" className="p-2 w-[180px] h-[172px] md:w-[250px] md:h-[240px] shadow-xl md:shadow-2xl rounded-full" />
+                            </a>
+                            <a href="https://github.com/aaronvidanalora/SRED" target="_blank" rel="noopener noreferrer">
                                 <p className="text-center text-xl font-semibold w-full md:text-2xl md:mt-4">Sistema de reservas</p>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                     <div className="rounded-full shadow-xl shadow-green-300 md:shadow-none hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <a href="https://tetris-eqvv.onrender.com/" target="_blank" rel="noopener noreferrer">
-                            <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
-                                <img src="/tetris.png" alt="proyecto" className="p-2 w-[180px] md:w-[250px] shadow-xl md:shadow-2xl rounded-full" />
+                        <div className="p-2 flex items-center w-[450px] md:flex md:flex-col md:items-center md:w-[300px] ">
+                            <a href="https://tetris-eqvv.onrender.com/" target="_blank" rel="noopener noreferrer">
+                                <img src="/tetris.png" alt="proyecto-tetris" className="p-2 w-[180px] md:w-[250px] shadow-xl md:shadow-2xl rounded-full" />
+                            </a>
+                            <a href="https://github.com/adriansanchezlarrondo/tetris_v1" target="_blank" rel="noopener noreferrer">
                                 <p className="text-center text-xl font-semibold w-full md:text-2xl md:mt-4">Tetris</p>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
